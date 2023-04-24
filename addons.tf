62c1ece62894272d3dfe617ce4af074484e6e994
Victor Gu <ec2-user@ip-10-1-1-13.us-west-2.compute.internal>
2023-04-24 15:17:47 +0000

    add-ons and sd chart

--- addons.tf
+++ addons.tf
@@ -82,8 +82,198 @@ module "eks_blueprints_kubernetes_addons" {
     version = "1.4.7"
     timeout = "300"
   }
+  
+  #---------------------------------------
+  # Amazon Managed Prometheus
+  #---------------------------------------
+  enable_amazon_prometheus             = true
+  amazon_prometheus_workspace_endpoint = aws_prometheus_workspace.amp.prometheus_endpoint
+
+  #---------------------------------------
+  # Prometheus Server Add-on
+  #---------------------------------------
+  enable_prometheus = true
+  prometheus_helm_config = {
+    name       = "prometheus"
+    repository = "https://prometheus-community.github.io/helm-charts"
+    chart      = "prometheus"
+    version    = "15.10.1"
+    namespace  = "prometheus"
+    timeout    = "300"
+    values     = [templatefile("${path.module}/helm-values/prometheus-values.yaml", {})]
+  }
+  
 
   tags = local.tags
+}
+
+#---------------------------------------------------------------
+# Amazon Prometheus Workspace
+#---------------------------------------------------------------
+resource "aws_prometheus_workspace" "amp" {
+  alias = format("%s-%s", "amp-ws", local.cluster_name)
+  tags  = local.tags
+}
+
+#---------------------------------------------------------------
+# Karpenter add-on
+#---------------------------------------------------------------
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "KarpenterNodeInstanceProfile-${local.cluster_name}"
+  role = module.eks.eks_managed_node_groups["karpenter"].iam_role_name
+
+  depends_on = [
+    module.eks
+  ]
+}
+
+module "karpenter_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 4.21.1"
+
+  role_name                          = "karpenter-controller-${local.cluster_name}"
+  attach_karpenter_controller_policy = true
+
+  karpenter_controller_cluster_id = module.eks.cluster_id
+  karpenter_controller_ssm_parameter_arns = [
+    "arn:${local.partition}:ssm:*:*:parameter/aws/service/*"
+  ]
+  karpenter_controller_node_iam_role_arns = [
+    module.eks.eks_managed_node_groups["karpenter"].iam_role_arn
+  ]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["karpenter:karpenter"]
+    }
+  }
+}
+
+
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = "v0.13.2"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter_irsa.iam_role_arn
+  }
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_id
+  }
+
+  set {
+    name  = "clusterEndpoint"
+    value = module.eks.cluster_endpoint
+  }
+
+  set {
+    name  = "aws.defaultInstanceProfile"
+    value = aws_iam_instance_profile.karpenter.name
+  }
+}
+
+#---------------------------------------
+# Karpenter Provisioners
+#---------------------------------------
+
+resource "kubectl_manifest" "karpenter_provisioner_gpu" {
+  yaml_body = <<-YAML
+  apiVersion: karpenter.sh/v1alpha5
+  kind: Provisioner
+  metadata:
+    name: gpu
+  spec:
+    ttlSecondsAfterEmpty: 300
+    labels:
+      jina.ai/node-type: gpu
+      jina.ai/gpu-type: nvidia
+      nvidia.com/gpu.present: true
+    requirements:
+      - key: node.kubernetes.io/instance-type
+        operator: In
+        values: ["g4dn.xlarge", "g4dn.2xlarge", "g4dn.4xlarge", "g4dn.12xlarge"]
+      - key: karpenter.sh/capacity-type
+        operator: In
+        values: ["spot", "on-demand"]
+      - key: kubernetes.io/arch
+        operator: In
+        values: ["amd64"]
+    taints:
+      - key: nvidia.com/gpu
+        effect: "NoSchedule"
+    limits:
+      resources:
+        cpu: 1000
+    provider:
+      launchTemplate: "karpenter-gpu-${local.cluster_name}"
+      subnetSelector:
+        karpenter.sh/discovery: ${local.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${local.cluster_name}
+    ttlSecondsAfterEmpty: 30
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+
+resource "aws_launch_template" "gpu" {
+  name = "karpenter-gpu-${local.cluster_name}"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 120
+    }
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.karpenter.name
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      "karpenter.sh/discovery" = local.cluster_name
+      "jina.ai/node-type"      = "gpu"
+    }
+  }
+
+  image_id = data.aws_ami.eks_node_gpu.image_id
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  update_default_version = true
+
+  # key_name = "${local.cluster_name}-sshkey"
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "optional"
+    http_put_response_hop_limit = 2
+  }
+
+  vpc_security_group_ids = [module.eks.node_security_group_id]
+
+  user_data = base64encode(templatefile("${path.module}/customized_bootstraps.sh", { cluster_name = "${local.cluster_name}" }))
+
+  tags = {
+    "karpenter.sh/discovery" = local.cluster_name
+    "node-type"              = "gpu"
+  }
 }
 
 
